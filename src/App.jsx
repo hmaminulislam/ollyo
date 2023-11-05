f9b751620b0ae61084a53808887ad2ad8f46193b
hmaminulislam <mdaminulislamsahin14@gmail.com>
2023-11-05 22:48:12 +0600

    upload image and delete image implement

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import {
   closestCenter,
   DndContext,
@@ -14,27 +14,30 @@ import {
   rectSortingStrategy,
 } from "@dnd-kit/sortable";
 import { SortableItem } from "./components/SortableItem";
-import "./App.css";
+import './App.css'
 
+// gallery json dummy data
+let data = [
+  { img: "https://i.ibb.co/M6jnfdH/image-11.jpg", id: "1" },
+  { img: "https://i.ibb.co/Fb3Lkbd/image-10.jpg", id: "2" },
+  { img: "https://i.ibb.co/qB2jxjn/image-9.webp", id: "3" },
+  { img: "https://i.ibb.co/M7vTjXj/image-8.webp", id: "4" },
+  { img: "https://i.ibb.co/1GjK3jx/image-7.webp", id: "5" },
+  { img: "https://i.ibb.co/j60RSx4/image-6.webp", id: "6" },
+  { img: "https://i.ibb.co/cc06Yrm/image-5.webp", id: "7" },
+  { img: "https://i.ibb.co/tmPrpP6/image-4.webp", id: "8" },
+  { img: "https://i.ibb.co/ZmZMmLz/image-3.webp", id: "9" },
+  { img: "https://i.ibb.co/0XWTN5z/image-2.webp", id: "10" },
+  { img: "https://i.ibb.co/4WGpmLh/image-1.webp", id: "11" },
+];
 function App() {
   const [activeId, setActiveId] = useState(null);
-
-  // data
-  const [languages, setLanguages] = useState([
-    "https://i.ibb.co/4WGpmLh/image-1.webp",
-    "https://i.ibb.co/0XWTN5z/image-2.webp",
-    "https://i.ibb.co/ZmZMmLz/image-3.webp",
-    "https://i.ibb.co/tmPrpP6/image-4.webp",
-    "https://i.ibb.co/cc06Yrm/image-5.webp",
-    "https://i.ibb.co/j60RSx4/image-6.webp",
-    "https://i.ibb.co/1GjK3jx/image-7.webp",
-    "https://i.ibb.co/M7vTjXj/image-8.webp",
-    "https://i.ibb.co/qB2jxjn/image-9.webp",
-    "https://i.ibb.co/Fb3Lkbd/image-10.jpg",
-    "https://i.ibb.co/M6jnfdH/image-11.jpg",
-  ]);
-
-  // dnd
+  const [images, setImages] = useState(data);
+  const [activeDrag, setActiveDrag] = useState(false);
+  const [selectedData, setSelectedData] = useState();
+  const [dataRemove, setDataRemove] = useState(false);
+  const itemIds = useMemo(() => images.map((item) => item.img), [images]);
+  // dnd kit
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -44,6 +47,7 @@ function App() {
 
   //  handle drag start
   function handleDragStart(event) {
+    setActiveDrag(true);
     const { active } = event;
     setActiveId(active.id);
   }
@@ -51,21 +55,63 @@ function App() {
   // handle drag end
   function handleDragEnd(event) {
     const { active, over } = event;
+    setActiveDrag(false);
     if (active.id !== over.id) {
-      setLanguages((languages) => {
-        const oldIndex = languages.indexOf(active.id);
-        const newIndex = languages.indexOf(over.id);
+      setImages((images) => {
+        const oldIndex = images.findIndex((item) => item.img === active.id);
+        const newIndex = images.findIndex((item) => item.img === over.id);
 
-        return arrayMove(languages, oldIndex, newIndex);
+        return arrayMove(images, oldIndex, newIndex);
       });
+    } else {
+      return;
     }
     setActiveId(null);
   }
 
+  // handle image upload
+  const handleUploadImg = (event) => {
+    const imgUrl = URL.createObjectURL(event.target.files[0]);
+    const id = (images.length + 1).toString();
+    const newData = { img: imgUrl, id: id };
+    setImages([...images, newData]);
+  }
+
+  // handle delete
+  const handleDelete = () => {
+    setSelectedData(false);
+    const currentData = data.filter((s) => {
+      return !selectedData.includes(s.id);
+    });
+    setImages(currentData);
+    setDataRemove(true);
+  }
+  
   return (
     <div className="container">
       <div className="gallery-top">
-        <h3 className="gallery-title">Gallery</h3>
+        {selectedData?.length ? (
+          <div className="selecte-title-wrapper">
+            <input
+              type="checkbox"
+              className="check-input"
+              checked={true}
+              readOnly
+            />
+            <h3 className="gallery-title">
+              {selectedData?.length} Files Selected
+            </h3>
+          </div>
+        ) : (
+          <h3 className="gallery-title">Gallery</h3>
+        )}
+        {selectedData?.length ? (
+          <button onClick={handleDelete} className="delete-btn">
+            Delete Files
+          </button>
+        ) : (
+          ""
+        )}
       </div>
       <DndContext
         sensors={sensors}
@@ -73,13 +119,37 @@ function App() {
         onDragStart={handleDragStart}
         onDragEnd={handleDragEnd}
       >
-        <div className="content">
-          <SortableContext items={languages} strategy={rectSortingStrategy}>
-            {languages.map((language) => (
-              <SortableItem key={language} id={language} />
-            ))}
-          </SortableContext>
-        </div>
+        <form>
+          <div className="content">
+            <SortableContext items={itemIds} strategy={rectSortingStrategy}>
+              {images.map((item) => (
+                <SortableItem
+                  key={item.id}
+                  id={item.img}
+                  value={item.id}
+                  activeDrag={activeDrag}
+                  setSelectedData={setSelectedData}
+                  dataRemove={dataRemove}
+                  setDataRemove={setDataRemove}
+                />
+              ))}
+            </SortableContext>
+            <div className="add-img-wrapper">
+              <input
+                className="file-input"
+                onChange={handleUploadImg}
+                accept="image/*"
+                type="file"
+              />
+              <img
+                className="upload-img-icon"
+                src="https://i.ibb.co/CHWyx6r/image-icon.png"
+                alt=""
+              />
+              <h4 className="add-img-title">Add Images</h4>
+            </div>
+          </div>
+        </form>
       </DndContext>
     </div>
   );
--- src/components/SortableItem.jsx
+++ src/components/SortableItem.jsx
@@ -1,19 +1,96 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
+// checkbox selected data 
+let data = [];
+
 export function SortableItem(props) {
-  // dnd style
   const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: props.id });
+    useSortable({ id: props.id })
+  // dnd kit style 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
   };
 
+  // select dom image and check field
+  const imageOverley = useRef();
+  const checkField = useRef();
+  const galleryImg = useRef();
+
+  // handle mouse over in gallery image
+  const handleMouseOver = () => {
+    imageOverley.current.style.display = "block";
+    checkField.current.style.display = "block";
+  };
+
+  // handle mouse out in gallery image
+  const handleMouseOut = () => {
+    const value = checkField.current.value;
+    const seletItem = data.find((item) => item == value);
+
+    if (!seletItem) {
+      imageOverley.current.style.display = "none";
+      checkField.current.style.display = "none";
+    }
+  };
+
+  // handle on drag in gallery image
+  const activeDrag = () => {
+    if (props.activeDrag) {
+      const over = imageOverley.current.id;
+      const existData = data.find((item) => item == over);
+      if (!existData) {
+        imageOverley.current.style.display = "none";
+        checkField.current.style.display = "none";
+      }
+    }
+  };
+  activeDrag();
+
+  // handle Checkbox
+  const handleCheck = (e) => {
+    props.setDataRemove(false);
+
+    const value = e.target.value.toString();
+    const existingData = data.find((item) => item == value);
+    const removeData = data.filter((item) => item != value);
+
+    if (existingData) {
+      imageOverley.current.style.opacity = 1;
+      galleryImg.current.style.opacity = 1;
+      data = removeData;
+    } else {
+      imageOverley.current.style.opacity = 0.2;
+      galleryImg.current.style.opacity = 0.5;
+      data = [...data, value];
+    }
+    props.setSelectedData(data);
+  };
+
+  // delete data remove from checkbox selected data
+  if (props.dataRemove) {
+    data = [];
+  }
+
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      <img className="gallary-img" src={props.id} alt="" />
+    <div
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+      className="gallery-img-wrapper"
+    >
+      <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+        <div ref={imageOverley} id={props.value} className="overley-img"></div>
+        <img ref={galleryImg} className="gallery-img" src={props.id} alt="" />
+      </div>
+      <input
+        type="checkbox"
+        ref={checkField}
+        value={props.value}
+        onChange={handleCheck}
+        className="check-input-img"
+      />
     </div>
   );
 }
